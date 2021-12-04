369838ed8543ae0ee22ce2a44a5a323ad2186d07
Sheikh Rashed <48760920+SheikhRashed@users.noreply.github.com>
2021-12-04 20:06:24 +0600

    Simple Array of heading added

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 // const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
-//  Array Methods  \\
+//  Array Methods ()  \\
 
 /* 
-=======
-
+====================
+1. Slice () 
+2. Splice()
+3. Reverse()
+4. Concat()
+5. Join()
+6. 
+====================
 */
